0406c53393a23878e1d2e9481ae0e9fc0acb76be
neeraj1bh <neeraj1bh@gmail.com>
2020-02-06 14:59:25 +0530

    Implementation of Valid Parenthesis with the help of Stack.

---
+++ Stack and Queue/Valid Parenthesis.py
@@ -0,0 +1,46 @@
+from Stack import Stack
+
+def is_valid(expr):
+    st = Stack()
+
+    for ch in expr:
+        if ch in '({[':
+            st.push(ch)
+        if ch in ')}]':
+            if st.is_empty():
+                print("Right parentheses are more than left parentheses")
+                return False
+            else:
+                char = st.pop()
+                if not match_parentheses(char, ch):
+                    print("Mismatched parentheses are ", char," and ", ch)
+                    return False
+
+    if st.is_empty():
+        print("Balanced parentheses")
+        return True
+    else:
+        print("Left parentheses are more than right parentheses")
+        return False
+
+def match_parentheses(leftPar, rightPar):
+    if leftPar == '[' and rightPar == ']':
+        return True
+    if leftPar == '{' and rightPar == '}':
+        return True
+    if leftPar == '(' and rightPar == ')':
+        return True
+    return False
+
+#################################################
+
+while True:
+    print("Enter an expression with parentheses (q to quit) : ", end='')
+    expression = input()
+    if expression == "q":
+        break
+
+    if is_valid(expression):
+        print("Valid expression")
+    else:
+        print("Invalid expression")
